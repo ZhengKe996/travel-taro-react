bad264bfa83d0ec345a110e17f2ae1bb718e1825
ZhengKe <3119531802@qq.com>
2022-06-19 19:26:04 +0800

    feat: 全局样式

--- src/pages/flight/index/index.js
+++ src/pages/flight/index/index.js
@@ -1,5 +1,5 @@
 import { PureComponent } from "react";
-import { View, SwiperItem } from "@tarojs/components";
+import { View, SwiperItem, Swiper, Image } from "@tarojs/components";
 import "./index.scss";
 import Tab from "../../../components/Tab";
 
@@ -7,6 +7,10 @@ const FLIGHT_TABS = [
   { label: "单程", id: 0 },
   { label: "多程", id: 1 },
   { label: "往返", id: 2 },
+];
+const BANNER_IMGS = [
+  "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/iwork/banner/01.png",
+  "https://zhengke-img.oss-cn-hangzhou.aliyuncs.com/iwork/banner/02.png",
 ];
 export default class FightIndex extends PureComponent {
   handleTabClick = (id) => {
@@ -26,6 +30,15 @@ export default class FightIndex extends PureComponent {
             <SwiperItem>222</SwiperItem>
           </Tab>
         </View>
+        <Swiper className="advs-banner-bd" autoplay circular interval={3000}>
+          {BANNER_IMGS.map((item) => {
+            return (
+              <SwiperItem key={item} className="item">
+                <Image className="img" src={item}></Image>
+              </SwiperItem>
+            );
+          })}
+        </Swiper>
       </View>
     );
   }
